85434e053ce03859acc69582db320365839da67e
Isabella Cadavid Posada <icadavidp@eafit.edu.co>
2026-02-06 23:18:12 -0500

    Update about view to use about template

--- movie/views.py
+++ movie/views.py
@@ -10,4 +10,6 @@ def home(request):
     return render (request, 'home.html', {'Name': 'This is Isabella Cadavid Posada'}) # Se puede pasar un diccionario con datos a la plantilla para que se muestren en la página. En este caso, se pasa el nombre "Isabella" para que se muestre en la página.
 
 def about(request):
-    return HttpResponse("<h1>About Movie Reviews</h1><p>This is a website where you can find reviews of movies.</p>")
+
+    #return HttpResponse("<h1>About Movie Reviews</h1><p>This is a website where you can find reviews of movies.</p>") - Antes, se muestra un mensaje simple con HttpResponse.
+    return render(request, 'about.html') # Después, se crea un archivo about.html en la carpeta templates y se llama a ese archivo desde la función about. De esta manera, se puede crear una página más completa y personalizada.

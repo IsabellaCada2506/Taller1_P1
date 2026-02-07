588ffc5e38473cf77db4def0cae11c58c2bdae0e
Isabella Cadavid Posada <icadavidp@eafit.edu.co>
2026-02-06 23:06:15 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -7,7 +7,7 @@ def home(request):
 
     #  return HttpResponse("<h1>Welcome to the Movie Reviews Home Page!</h1>") - Antes
     #return render (request, 'home.html') # Después, se crea un archivo home.html en la carpeta templates y se llama a ese archivo desde la función home. De esta manera, se puede crear una página más completa y personalizada.
-    return render (request, 'home.html', {'Name': 'This is Isabella Cadavid'}) # Se puede pasar un diccionario con datos a la plantilla para que se muestren en la página. En este caso, se pasa el nombre "Isabella" para que se muestre en la página.
+    return render (request, 'home.html', {'Name': 'This is Isabella Cadavid Posada'}) # Se puede pasar un diccionario con datos a la plantilla para que se muestren en la página. En este caso, se pasa el nombre "Isabella" para que se muestre en la página.
 
 def about(request):
     return HttpResponse("<h1>About Movie Reviews</h1><p>This is a website where you can find reviews of movies.</p>")
